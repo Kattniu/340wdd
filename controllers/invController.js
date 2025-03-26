ee70279c02097e4a54eed6e314a253ae55880755
Kattniu <kathe.adriana24@gmail.com>
2025-03-26 13:12:22 +0100

    week03/04

---
+++ controllers/invController.js
@@ -0,0 +1,42 @@
+const invModel = require("../models/inventory-model");
+const utilities = require("../utilities/");
+
+const invCont = {};
+
+/* ***************************
+ *  Build inventory by classification view
+ * ************************** */
+invCont.buildByClassificationId = async function (req, res, next) {
+  const classification_id = req.params.classificationId;
+  const data = await invModel.getInventoryByClassificationId(classification_id);
+  const grid = await utilities.buildClassificationGrid(data);
+  let nav = await utilities.getNav();
+  const className = data[0].classification_name;
+  res.render("./inventory/classification", {
+    title: className + " vehicles",
+    nav,
+    grid,
+  });
+};
+
+/* ***************************
+ *  Build vehicle detail view
+ * ************************** */
+invCont.buildItemDetailById = async function (req, res, next) {
+  try {
+    const inv_id = req.params.inv_id;
+    const vehicleData = await invModel.getInventoryItemById(inv_id);
+    const vehicleHTML = await utilities.buildItemDetails(vehicleData);
+    let nav = await utilities.getNav();
+    res.render("./inventory/vehicle-details", {
+      title: `${vehicleData.inv_make} ${vehicleData.inv_model}`,
+      nav,
+      vehicleHTML,
+    });
+  } catch (error) {
+    console.error("Error building vehicle detail view");
+    next(error);
+  }
+};
+
+module.exports = invCont;
